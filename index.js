d493074f91a9d2529ddcb4b3976b4290e1c6124d
MARK-GHOBRIAL <Mark_emad8@yahoo.com>
2024-11-12 16:45:33 +0100

    adds

---
+++ index.js
@@ -0,0 +1,7 @@
+const buttonInvia = document.getElementById('invia')
+const buttonReset = document.getElementById('reset')
+const textArea = document.getElementById('text')
+
+buttonInvia.addEventListener('click', function(){
+    
+})
